9346af576d1ad3dd3f2bd65fb7887e205972fe55
Sebastian Zok <sebastianpaulzok@gmail.com>
2021-05-20 23:35:37 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,77 @@
+function main() {
+
+    var scene = new THREE.Scene();
+    var box = generateBox(1, 1, 1);
+
+    box.translateZ(-(box.geometry.parameters.height / 2));
+    var floor = generateFloor(10, 10);
+    floor.name = 'floor';
+
+    floor.rotation.x = Math.PI / 2;
+
+    scene.add(floor);
+    floor.add(box);
+    var plight = generatePointLight(0xffffff, 1);
+    plight.position.y = 5;
+    scene.add(plight);
+
+
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / innerHeight, 1, 1000);               // FOV / RATIO / CLIPPING-PLANES
+    camera.position.x = 1;
+    camera.position.y = 5;
+    camera.position.z = 5;
+    camera.lookAt(new THREE.Vector3(0, 0, -5));
+
+    var renderer = new THREE.WebGLRenderer();   // WebGL REnderer nutzt Graka
+
+    renderer.shadowMap.enabled = true;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor('rgb(60,150,60)')
+    document.getElementById('webgl').appendChild(renderer.domElement);
+    update(renderer, scene, camera);
+}
+
+function generateFloor(w, d) {
+
+    var geo = new THREE.PlaneGeometry(w, d);
+    var mat = new THREE.MeshPhongMaterial({ color: 'rgb(100,100,100)', side: THREE.DoubleSide });
+    var mesh = new THREE.Mesh(geo, mat);
+    mesh.receiveShadow = true;
+    return mesh;
+
+}
+
+
+function generatePointLight(color, intensity) {
+    var light = new THREE.PointLight(color, intensity);
+    light.castShadow = true;
+    
+    return light
+}
+
+
+function generateBox(w, h, d) {
+    var geo = new THREE.BoxGeometry(w, h, d);
+    var mat = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    var mesh = new THREE.Mesh(geo, mat);
+    mesh.castShadow = true;
+    return mesh;
+}
+
+
+function update(renderer, scene, camera) {
+    renderer.render(scene, camera);
+
+    scene.traverse(function (child) {
+
+        // child.rotation.z += 0.01;
+
+    })
+
+
+    requestAnimationFrame(function () { update(renderer, scene, camera) });
+
+}
+
+
+main();
